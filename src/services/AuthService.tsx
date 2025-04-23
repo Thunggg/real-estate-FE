92614f6657656425029ee33e603ad811448767b0
Thunggg <thuannguyen20041028@gmail.com>
2025-04-24 04:45:47 +0700

    thêm jwt cho đăng nhập

--- src/services/AuthService.tsx
+++ src/services/AuthService.tsx
@@ -1,3 +1,4 @@
+import Password from "antd/es/input/Password";
 import { LoginAPI } from "../config/api";
 import axiosInstance from "../config/axios.customize"
 
@@ -7,8 +8,11 @@ type PayloadLogin = {
 };
 
 const LoginService = (payload: PayloadLogin) => {
-    const res = axiosInstance.post('/login');
-    console.log(res);
+    const res = axiosInstance.post('/auth/login', {
+        email: payload.email,
+        password: payload.password
+    });
+    console.log(payload.email, payload.password);
 }
 
 export { LoginService }
